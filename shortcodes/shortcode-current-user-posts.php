793e8896a6fbb0334a23e8ab6283374c467ba8e2
Remco Tolsma <info@remcotolsma.nl>
2012-11-13 13:29:27 +0100

    WordPress Coding Standards optimizations.

--- shortcodes/shortcode-current-user-posts.php
+++ shortcodes/shortcode-current-user-posts.php
@@ -3,42 +3,42 @@
 /**
  * Shortcode current user posts
  */
-function pronamic_framework_current_user_posts($atts, $content = null) {
+function pronamic_framework_current_user_posts( $atts, $content = null ) {
 	$result = '';
 
 	// Globals set
 	$GLOBALS['pronamic_framework_is_current_user_posts'] = true;
 
-	if(is_user_logged_in()) {
-		extract(shortcode_atts(array(
+	if ( is_user_logged_in() ) {
+		extract( shortcode_atts( array(
 			'query' => array()
-		), $atts));
+		), $atts ) );
 
 		$user = wp_get_current_user();
 
 		// Query args
-		if(is_string($query)) {
-			$query = html_entity_decode($query);
+		if ( is_string( $query ) ) {
+			$query = html_entity_decode( $query );
 		}
 
-		$queryArgs = wp_parse_args($query, array(
-			'author' => $user->ID , 
+		$query_args = wp_parse_args( $query, array(
+			'author'         => $user->ID, 
 			'posts_per_page' => -1
-		));
+		) );
 
 		// Query start
 		global $wp_query;
 
 		$original_query = $wp_query;
 		$wp_query = null;
-		$wp_query = new WP_Query($queryArgs);
+		$wp_query = new WP_Query( $query_args );
 
 		// Template
 		ob_start();
 		
-		$template = plugin_dir_path(Pronamic_Framework::$file) . '/templates/current-user-posts.php';
+		$template = plugin_dir_path( Pronamic_Framework::$file ) . '/templates/current-user-posts.php';
 
-		load_template($template, false);
+		load_template( $template, false );
 		
 		$result = ob_get_clean();
 
@@ -49,12 +49,12 @@ function pronamic_framework_current_user_posts($atts, $content = null) {
 	}
 
 	// Globals unset
-	unset($GLOBALS['pronamic_framework_is_current_user_posts']);
+	unset( $GLOBALS['pronamic_framework_is_current_user_posts'] );
 
 	return $result;
 }
 
-add_shortcode('pronamic_current_user_posts', 'pronamic_framework_current_user_posts');
+add_shortcode( 'pronamic_current_user_posts', 'pronamic_framework_current_user_posts' );
 
 function pronamic_framework_is_current_user_posts() {
 	global $pronamic_framework_is_current_user_posts;
@@ -63,21 +63,21 @@ function pronamic_framework_is_current_user_posts() {
 }
 
 /**
+ * Get edit post link
  * 
- * Enter description here ...
- * @param unknown_type $url
- * @param unknown_type $postId
- * @param unknown_type $context
+ * @param string $url
+ * @param string $postId
+ * @param string $context
  */
-function pronamic_framework_get_edit_post_link($url, $postId, $context) {
-	if(pronamic_framework_is_current_user_posts()) {
-		$editPostPageId = get_option('pronamic_framework_edit_post_page_id');
+function pronamic_framework_get_edit_post_link( $url, $post_id, $context ) {
+	if ( pronamic_framework_is_current_user_posts() ) {
+		$edit_post_page_id = get_option( 'pronamic_framework_edit_post_page_id' );
 
-		if(!empty($editPostPageId)) {
-			$link = get_permalink($editPostPageId);
+		if ( !empty( $edit_post_page_id ) ) {
+			$link = get_permalink( $edit_post_page_id );
 
-			if($link) {
-				$url = add_query_arg('post', $postId, $link);
+			if ( $link ) {
+				$url = add_query_arg( 'post', $post_id, $link );
 			}
 		}
 	}
@@ -85,4 +85,4 @@ function pronamic_framework_get_edit_post_link($url, $postId, $context) {
 	return $url;
 }
 
-add_filter('get_edit_post_link', 'pronamic_framework_get_edit_post_link', 10, 3);
+add_filter( 'get_edit_post_link', 'pronamic_framework_get_edit_post_link', 10, 3 );
--- templates/current-user-posts.php
+++ templates/current-user-posts.php
@@ -1,45 +1,45 @@
-<?php if(have_posts()): ?>
-
-<table>
-	<thead>
-		<tr>
-			<th scope="col">
-				<?php _e('Title', 'pronamic_framework'); ?>
-			</th>
-			<th scope="col">
-				<?php _e('Date', 'pronamic_framework'); ?>
-			</th>
-			<th scope="col">
-				<?php _e('Actions', 'pronamic_framework'); ?>
-			</th>
-		</tr>
-	</thead>
-
-	<tbody>
-
-		<?php while(have_posts()): the_post(); ?>
-
-		<tr>
-			<td>
-				<a href="<?php the_permalink(); ?>">
-					<?php the_title(); ?>
-				</a>
-			</td>
-			<td>
-				<?php the_time(get_option('date_format')); ?>
-			</td>
-			<td>
-				<?php edit_post_link(); ?>
-			</td>
-		</tr>
-
-		<?php endwhile; ?>
-
-	</tbody>
-</table>
+<?php if ( have_posts() ): ?>
+	
+	<table>
+		<thead>
+			<tr>
+				<th scope="col">
+					<?php _e( 'Title', 'pronamic_framework' ); ?>
+				</th>
+				<th scope="col">
+					<?php _e( 'Date', 'pronamic_framework' ); ?>
+				</th>
+				<th scope="col">
+					<?php _e( 'Actions', 'pronamic_framework' ); ?>
+				</th>
+			</tr>
+		</thead>
+	
+		<tbody>
+	
+			<?php while ( have_posts() ): the_post(); ?>
+		
+				<tr>
+					<td>
+						<a href="<?php the_permalink(); ?>">
+							<?php the_title(); ?>
+						</a>
+					</td>
+					<td>
+						<?php the_time( get_option( 'date_format' ) ); ?>
+					</td>
+					<td>
+						<?php edit_post_link(); ?>
+					</td>
+				</tr>
+	
+			<?php endwhile; ?>
+	
+		</tbody>
+	</table>
 
 <?php else: ?>
 
-
+	
 
 <?php endif; ?>
