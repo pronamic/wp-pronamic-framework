6b55df0c6baa3c3fcdebae79336e0a0e5dd418cf
Remco Tolsma <info@remcotolsma.nl>
2014-10-10 16:41:44 +0200

    WordPress Coding Standards optimizations.

--- functions/user-image.php
+++ functions/user-image.php
@@ -1,32 +1,32 @@
 <?php
 
-function pronamic_user_image($user) {
+function pronamic_user_image( $user ) {
 	?>
-	<h3><?php _e('Media', 'pronamic_framework'); ?></h3>
+	<h3><?php _e( 'Media', 'pronamic_framework' ); ?></h3>
 
 	<table class="form-table">
 		<tr>
 			<th>
 				<label for="user_image">
-					<?php _e('Profile Image', 'pronamic_framework'); ?>
+					<?php _e( 'Profile Image', 'pronamic_framework' ); ?>
 				</label>
 			</th>
 			<td>
 				<?php pronamic_media_select_field( '_pronamic_image_id' , pronamic_get_user_image_id( $user->ID ) ); ?>
-				<span class="description"><?php _e('Select an image from the media library. This may be shown publicly.', 'pronamic_framework'); ?></span>
+				<span class="description"><?php _e( 'Select an image from the media library. This may be shown publicly.', 'pronamic_framework' ); ?></span>
 			</td>
 		</tr>
 	</table>
 	<?php
 }
 
-add_action('show_user_profile', 'pronamic_user_image');
-add_action('edit_user_profile', 'pronamic_user_image');
+add_action( 'show_user_profile', 'pronamic_user_image' );
+add_action( 'edit_user_profile', 'pronamic_user_image' );
 
-function pronamic_save_user_image($user_id) {
-	$image_id = filter_input(INPUT_POST, '_pronamic_image_id', FILTER_VALIDATE_INT);
+function pronamic_save_user_image( $user_id ) {
+	$image_id = filter_input( INPUT_POST, '_pronamic_image_id', FILTER_VALIDATE_INT );
 
-	if( empty ( $image_id ) ) {
+	if ( empty( $image_id ) ) {
 		delete_user_meta( $user_id , '_pronamic_image_id' );
 	} else {
 		update_user_meta( $user_id, '_pronamic_image_id', $image_id );
@@ -34,6 +34,5 @@ function pronamic_save_user_image($user_id) {
 }
 
 
-add_action('personal_options_update', 'pronamic_save_user_image');
-add_action('edit_user_profile_update', 'pronamic_save_user_image');
-
+add_action( 'personal_options_update', 'pronamic_save_user_image' );
+add_action( 'edit_user_profile_update', 'pronamic_save_user_image' );
